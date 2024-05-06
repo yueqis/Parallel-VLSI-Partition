173ff69f18c2108a95984c518aa4220da741e1a8
jhensyuw <136679846+jhensyuw@users.noreply.github.com>
2024-05-06 02:06:43 -0400

    Delete KL/test.sh

--- KL/test.sh
+++
@@ -1,4 +0,0 @@
-mpirun -n 1 ./bin/fm ../inputs/input_5000_20000.txt outputs/output_5000_20000.txt
-mpirun -n 2 ./bin/fm ../inputs/input_5000_20000.txt outputs/output_5000_20000.txt
-mpirun -n 4 ./bin/fm ../inputs/input_5000_20000.txt outputs/output_5000_20000.txt
-mpirun -n 8 ./bin/fm ../inputs/input_5000_20000.txt outputs/output_5000_20000.txt
